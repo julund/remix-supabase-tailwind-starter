00f415f15284d3ad89898c45958abeedfe0c375f
julund <julu900@gmail.com>
2022-12-23 14:58:42 +0000

    change style

--- app/layout/footer.tsx
+++ app/layout/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="p-6 text-base-600 bg-base-100 rounded-sm">
+        <footer className="p-6 text-base-600 text-sm bg-base-100 rounded-sm">
             Copyright (c) 2022 Remix starter
         </footer>
     )
--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
         <header className="flex p-6 gap-2 bg-base-100 rounded-sm">
             <NavLink className="shrink font-semibold" to="/">Remix starter</NavLink>
             <nav className="flex gap-4 grow justify-end">
-                <NavLink to="/lorem">Lorem</NavLink>
-                <NavLink to="/ipsum">Ipsum</NavLink>
+                <NavLink to="/lorem" className={ ({isActive}) => isActive ? "text-base-500" : "text-primary-600 hover:underline" }>Lorem</NavLink>
+                <NavLink to="/ipsum" className={ ({isActive}) => isActive ? "text-base-500" : "text-primary-600 hover:underline" }>Ipsum</NavLink>
             </nav>
         </header>
     )
