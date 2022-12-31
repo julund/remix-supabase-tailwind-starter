19e01b22e7ba4f7709660b903ff8f10d537d356c
Julian <julu900@gmail.com>
2022-12-31 14:25:14 +0100

    fix animation

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,10 +1,11 @@
 import clsx from "clsx";
 import { useRef } from "react";
-import { useClickAway, useToggle } from "react-use";
+import { useClickAway, useMeasure, useToggle } from "react-use";
 import { IconMenu, IconX } from "~/components/icon";
 
 export default function Nav({ children }: { children: React.ReactNode }) {
 
+    // const [ ref, { width }] = useMeasure<HTMLElement>();
     const buttonRef = useRef<HTMLButtonElement>(null);
     const dropDownRef = useRef<HTMLDivElement>(null);
     const [expanded, toggle] = useToggle(false);
@@ -17,15 +18,16 @@ export default function Nav({ children }: { children: React.ReactNode }) {
     return (
         <nav className="z-0 flex grow justify-end items-start relative py-2">
             <button ref={buttonRef} className="p-1 rounded-sm hover:bg-base-100 focus:bg-base-100 md:hidden" aria-label="toggle" onClick={toggle}>
-                {expanded ? <IconX className="text-base-800" /> : <IconMenu className="text-base-800" />}
+                {expanded ? <IconX className="text-base-800 pointer-events-none" /> : <IconMenu className="text-base-800 pointer-events-none" />}
             </button>
-                <div ref={dropDownRef} onClick={toggle} aria-labelledby="toggle" className={clsx(
-                    expanded ?
-                    "flex md:flex-row items-start flex-col flex-wrap gap-2 md:gap-4 absolute md:static top-10 rounded-sm md:bg-transparent bg-base-100 md:p-0 py-4 px-6" :
-                    "gap-4 hidden md:flex","duration-100"
-                    )}>
-                    {children}
-                </div>
+            <div ref={dropDownRef} onAnimationEnd={() => !expanded && dropDownRef.current?.classList.add("hidden")} onClick={toggle} aria-labelledby="toggle" className={clsx(
+                expanded ?
+                    "animate-fadeInDown md:animate-none" :
+                    "md:flex animate-fadeOutUp md:animate-none",
+                "flex items-start gap-2 md:gap-4 flex-col md:flex-row absolute md:static top-10 rounded-sm md:bg-transparent bg-base-100 md:p-0 py-4 px-6"
+            )}>
+                {children}
+            </div>
         </nav>
     );
 }
