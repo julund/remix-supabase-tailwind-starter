4dacb89e6ce2e5ce040a00ae596b9e55749448ee
Julian <julu900@gmail.com>
2023-01-02 18:31:08 +0100

    change style

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -4,8 +4,8 @@ import Nav from "~/components/nav";
 export default function Header() {
     return (
         <header className="sticky top-0 z-50 flex-grow-0 p-6 border-b-2 border-base-100 bg-base-50/50 ">
-            <div className="max-w-2xl mx-auto flex gap-2">
-                <NavLink className="shrink text-lg uppercase font-title font-black text-base-600 py-2" to="/">Remix Starter</NavLink>
+            <div className="max-w-2xl mx-auto flex gap-2 justify-between">
+                <NavLink className="text-lg uppercase font-title font-black text-base-600 py-2" to="/">Remix Starter</NavLink>
                 <Nav>
                     <NavLink to="/lorem" className={({ isActive }) => isActive ? "navlink active" : "navlink"}>Lorem</NavLink>
                     <NavLink to="/ipsum" className={({ isActive }) => isActive ? "navlink active" : "navlink"}>Ipsum</NavLink>
